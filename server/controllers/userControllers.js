5ae4fa8ee6c497384f7a6d81dbd1bf420523cfce
poornimasilote <poornimasilote1502@gmail.com>
2025-12-29 21:12:53 +0530

    Fix auth controller token generation

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -4,14 +4,14 @@ const createToken = require("../utilis/token")
 
 // Login user
 const loginUser= async(req,res)=>{
-     const {_id, email, password} = req.body;
+     const { email, password} = req.body;
     try{
          const user = await User.login(email, password)
 
          // create token
          const token = createToken(user._id)
 
-         res.status(200).json({email, password, token})
+         res.status(200).json({email, token})
 
     }catch(err){
         res.status(400).json({error:err.message})
@@ -20,14 +20,14 @@ const loginUser= async(req,res)=>{
 
 //Signup User
 const signupUser =async(req,res)=>{
-    const {_id, email, password} = req.body;
+    const { email, password} = req.body;
     try{
          const user = await User.signup(email, password)
 
          // create token
-         const token = createToken(_id)
+         const token = createToken(user._id)
 
-         res.status(200).json({email, password, token})
+         res.status(200).json({email, token})
 
     }catch(err){
         res.status(400).json({error:err.message})
