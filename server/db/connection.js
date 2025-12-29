1ba9de4c0e6735e1013a8d54f4b47a4faa5029b3
poornimasilote <poornimasilote1502@gmail.com>
2025-12-29 19:41:52 +0530

    Mongo db url changed from local host

--- server/db/connection.js
+++ server/db/connection.js
@@ -1,7 +1,7 @@
 const mongoose= require("mongoose")
 
 mongoose
-.connect("mongodb://localhost:27017/BackendProject")
+.connect(process.env.MONGO_URI)
 .then(()=>{
     console.log("Connection established")
 }).catch((err)=>{
