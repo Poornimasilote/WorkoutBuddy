1c8f4d2771b544349482c937da59bc1f4809565c
poornimasilote <poornimasilote1502@gmail.com>
2025-12-29 21:27:25 +0530

    workout controller.js fixed

--- server/controllers/workoutController.js
+++ server/controllers/workoutController.js
@@ -1,66 +1,131 @@
-const Workout = require("../models/workoutModel")
+const Workout = require("../models/workoutModel");
 
+/**
+ * GET all workouts (authenticated user only)
+ */
+const getWorkouts = async (req, res) => {
+  try {
+    // auth safety check
+    if (!req.user) {
+      return res.status(401).json({ error: "Not authorized" });
+    }
 
-// Get all Data 
+    const user_id = req.user._id;
 
-const getWorkouts = async(req,res) =>{
-    const user_id = req.user._id
+    const workouts = await Workout.find({ user_id })
+      .sort({ createdAt: -1 });
 
-      try{
-       const workoutData = await Workout.find({user_id}).sort({createdAt:-1})
-       res.status(200).json(workoutData)
-  }catch(err){
-      res.send(400).json({error:err.message})
+    res.status(200).json(workouts);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
   }
-}
+};
 
-//get single data 
-const getWorkout = async(req,res)=>{
-    try{
-        const id = req.params.id
-        const workoutData = await Workout.findById({_id:id})
-        res.status(200).json(workoutData)
-    }catch(err){
-        res.send(400).json({error:err.message})
+/**
+ * GET single workout (only if owned by user)
+ */
+const getWorkout = async (req, res) => {
+  try {
+    if (!req.user) {
+      return res.status(401).json({ error: "Not authorized" });
     }
-}
 
-// create data
-const createWorkout = async(req,res)=>{
-   const{title,reps, load}=  req.body
-   const user_id = req.user._id
-    try{
-        const newWorkout = new Workout({title,reps,load, user_id})
-        const workout = await newWorkout.save();
-        res.status(201).json(workout)
-    }catch(err){
-        res.send(400).json({error:err.message})
+    const workout = await Workout.findOne({
+      _id: req.params.id,
+      user_id: req.user._id
+    });
+
+    if (!workout) {
+      return res.status(404).json({ error: "Workout not found" });
     }
-}
 
-// update data
-const editWorkout = async (req, res)=>{
-    try{
-        const id = req.params.id
-    const workoutData = await Workout.findByIdAndUpdate({_id:id}, req.body, {new:true})
-    res.status(200).json(workoutData)
-    }catch(err){
-         res.send(400).json({error:err.message})
+    res.status(200).json(workout);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
+
+/**
+ * CREATE workout
+ */
+const createWorkout = async (req, res) => {
+  try {
+    if (!req.user) {
+      return res.status(401).json({ error: "Not authorized" });
     }
-}
 
-// delete data 
-const deleteWorkout = async(req,res)=>{
-    try{
-        const id = req.params.id
-        const workoutData = await Workout.findByIdAndDelete({_id:id})
-        res.status(200).json(workoutData)
-    }catch(err){
-        res.send(400).json({error:err.message})
+    const { title, reps, load } = req.body;
+
+    if (!title || !reps || !load) {
+      return res.status(400).json({ error: "All fields are required" });
     }
-}
 
+    const workout = await Workout.create({
+      title,
+      reps,
+      load,
+      user_id: req.user._id
+    });
+
+    res.status(201).json(workout);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
+
+/**
+ * UPDATE workout (only if owned by user)
+ */
+const editWorkout = async (req, res) => {
+  try {
+    if (!req.user) {
+      return res.status(401).json({ error: "Not authorized" });
+    }
+
+    const workout = await Workout.findOneAndUpdate(
+      { _id: req.params.id, user_id: req.user._id },
+      req.body,
+      { new: true }
+    );
+
+    if (!workout) {
+      return res.status(404).json({ error: "Workout not found" });
+    }
+
+    res.status(200).json(workout);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
+
+/**
+ * DELETE workout (only if owned by user)
+ */
+const deleteWorkout = async (req, res) => {
+  try {
+    if (!req.user) {
+      return res.status(401).json({ error: "Not authorized" });
+    }
+
+    const workout = await Workout.findOneAndDelete({
+      _id: req.params.id,
+      user_id: req.user._id
+    });
+
+    if (!workout) {
+      return res.status(404).json({ error: "Workout not found" });
+    }
+
+    res.status(200).json(workout);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
 
 module.exports = {
-    getWorkouts, getWorkout, createWorkout, editWorkout, deleteWorkout
-}
+  getWorkouts,
+  getWorkout,
+  createWorkout,
+  editWorkout,
+  deleteWorkout
+};
